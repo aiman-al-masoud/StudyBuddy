ca893e4ec7bf76af097584f7d5f9f6c60faea94f
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-07-24 00:41:43 +0200

    ...

--- app/src/main/java/com/luxlunaris/studybuddy/model/examiner/Examiner.java
+++ app/src/main/java/com/luxlunaris/studybuddy/model/examiner/Examiner.java
@@ -1,4 +1,14 @@
 package com.luxlunaris.studybuddy.model.examiner;
 
+import com.luxlunaris.studybuddy.model.challenge.Challenge;
+
 public class Examiner {
+
+    public String getReply(Challenge challenge, String userInput){
+//        return null;
+        throw new ArithmeticException();
+    }
+
+
+
 }
