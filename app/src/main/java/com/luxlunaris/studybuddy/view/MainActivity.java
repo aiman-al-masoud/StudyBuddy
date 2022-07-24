d15d3a80de523999d9dfc42080099b784da12137
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-07-24 18:44:46 +0200

    ...

--- app/src/main/java/com/luxlunaris/studybuddy/view/MainActivity.java
+++ app/src/main/java/com/luxlunaris/studybuddy/view/MainActivity.java
@@ -3,13 +3,18 @@ package com.luxlunaris.studybuddy.view;
 import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.PermissionChecker;
 
+import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
+import android.webkit.PermissionRequest;
+import android.widget.Toast;
 
 import com.luxlunaris.studybuddy.R;
 import com.luxlunaris.studybuddy.model.studybuddy.StudyBuddy;
+import com.luxlunaris.studybuddy.model.utils.Async;
 import com.luxlunaris.studybuddy.model.utils.FileManager;
 import com.luxlunaris.studybuddy.model.utils.Permissions;
 
@@ -25,19 +30,31 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Permissions.isPermissionGranted(this);
+        if(Permissions.isPermissionGranted(this)){
+            init();
+        }
+    }
 
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    private void init(){
         studyBuddy = new StudyBuddy(this);
         studyBuddy.addChallengesFile("foo", "Who built the pyramids?\\nAliens\\n\\nWhat do cats eat?\\n\\nTuna.");
         studyBuddy.start();
-
     }
 
     @Override
+    @RequiresApi(api = Build.VERSION_CODES.N)
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         Log.d("onRequestPermissionsRes", "onRequestPermissionsResult: "+requestCode+" "+ Arrays.asList(permissions) +" "+ Arrays.asList(grantResults));
         FileManager.createRootDir();
+
+        if(Arrays.stream(grantResults).allMatch(p-> p == PackageManager.PERMISSION_GRANTED)){
+            init();
+        }else{
+            Toast.makeText(this, "Sorry, you must grant all of the permissions to use the app.", Toast.LENGTH_LONG).show();
+            Async.setTimeout(()->System.exit(0), 3000);
+        }
     }
 
 
