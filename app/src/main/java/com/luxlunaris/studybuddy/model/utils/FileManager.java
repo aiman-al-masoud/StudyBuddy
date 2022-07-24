59bb329d93304df85f211343bf536690b902d386
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-07-24 11:07:26 +0200

    ...

--- app/src/main/java/com/luxlunaris/studybuddy/model/utils/FileManager.java
+++ app/src/main/java/com/luxlunaris/studybuddy/model/utils/FileManager.java
@@ -1,11 +1,16 @@
 package com.luxlunaris.studybuddy.model.utils;
 
+import android.content.Context;
+import android.net.Uri;
 import android.os.Environment;
 
 import java.io.BufferedWriter;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStream;
 
 public class FileManager {
 
@@ -29,14 +34,50 @@ public class FileManager {
         }
 
         f.createNewFile();
-        BufferedWriter br = new BufferedWriter(new FileWriter(f));
-        br.write(body);
+        FileWriter fr = new FileWriter(f);
+        fr.write(body);
+        fr.flush();
+        fr.close();
 
     }
 
-    public static String readTextFileFromRootDir(){
-        return "";
+
+    public static String uriToText(Context context, Uri uri) throws  IOException{
+
+        InputStream is = context.getContentResolver().openInputStream(uri);
+        ByteArrayOutputStream os = new ByteArrayOutputStream();
+
+        int i;
+        while ((i = is.read()) != -1 ){
+            os.write(i);
+        }
+
+        return os.toString();
     }
+
+
+    public static String readTextFileFromRootDir(String title) throws  IOException{
+        String path = getRootDirPath()+"/"+title+".txt";
+        File f = new File(path);
+
+        if(f.exists()){
+            FileReader fr = new FileReader(f);
+            ByteArrayOutputStream os = new ByteArrayOutputStream();
+
+            int i;
+            while ((i = fr.read()) != -1 ){
+                os.write(i);
+            }
+
+            return os.toString();
+        }
+
+        throw  new IOException();
+    }
+
+
+
+
 
 
 
