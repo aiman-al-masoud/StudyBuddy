57b574b271f0e120a91e5c153f49b4698633a1eb
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-07-24 11:51:21 +0200

    ...

--- app/src/main/java/com/luxlunaris/studybuddy/model/speaker/Speaker.java
+++ app/src/main/java/com/luxlunaris/studybuddy/model/speaker/Speaker.java
@@ -1,7 +1,14 @@
 package com.luxlunaris.studybuddy.model.speaker;
 
+import android.content.Context;
+
+import com.luxlunaris.studybuddy.model.speaker.classes.GoogleSpeaker;
+
 public interface Speaker {
 
     void speak(String speech);
 
+    static Speaker getSpeaker(Context context){
+        return new GoogleSpeaker(context);
+    }
 }
--- app/src/main/java/com/luxlunaris/studybuddy/model/studybuddy/StudyBuddy.java
+++ app/src/main/java/com/luxlunaris/studybuddy/model/studybuddy/StudyBuddy.java
@@ -2,12 +2,17 @@ package com.luxlunaris.studybuddy.model.studybuddy;
 
 import android.content.Context;
 
+import com.luxlunaris.studybuddy.model.examiner.Examiner;
 import com.luxlunaris.studybuddy.model.scribe.Scribe;
 import com.luxlunaris.studybuddy.model.scribe.ScribeListener;
+import com.luxlunaris.studybuddy.model.speaker.Speaker;
 
 public class StudyBuddy implements ScribeListener {
 
-    Context context;
+    private Context context;
+    private Examiner examiner;
+    private Scribe scribe;
+    private Speaker speaker;
 
 
     public StudyBuddy(Context context){
