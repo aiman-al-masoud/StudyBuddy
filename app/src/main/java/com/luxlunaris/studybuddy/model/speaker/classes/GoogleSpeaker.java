731284141a4c349c656a246465acc4a3859307d8
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-07-24 11:11:06 +0200

    ...

--- app/src/main/java/com/luxlunaris/studybuddy/model/speaker/classes/GoogleSpeaker.java
+++ app/src/main/java/com/luxlunaris/studybuddy/model/speaker/classes/GoogleSpeaker.java
@@ -1,13 +1,27 @@
 package com.luxlunaris.studybuddy.model.speaker.classes;
 
+import android.content.Context;
+import android.speech.tts.TextToSpeech;
+
 import com.luxlunaris.studybuddy.model.speaker.Speaker;
+
+import java.util.Locale;
 
 public class GoogleSpeaker implements Speaker {
 
+    private TextToSpeech tts;
+
+    public GoogleSpeaker(Context context){
+        tts = new TextToSpeech(context, e->{
+            //on init
+        });
+
+        tts.setLanguage(Locale.US);
+    }
 
     @Override
     public void speak(String speech) {
-
+        tts.speak(speech, TextToSpeech.QUEUE_FLUSH, null);
     }
 
 }
