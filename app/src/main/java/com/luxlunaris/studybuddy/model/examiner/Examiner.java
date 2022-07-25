84b949d36ba788ac1f3fe225dbbef9e38f853c59
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-07-25 10:50:23 +0200

    ...

--- app/src/main/java/com/luxlunaris/studybuddy/model/examiner/Examiner.java
+++ app/src/main/java/com/luxlunaris/studybuddy/model/examiner/Examiner.java
@@ -15,7 +15,6 @@ import java.util.stream.Collectors;
 
 public class Examiner {
 
-//    public final int EXCELLENT_ANSWER = 90;
     public final int GOOD_ANSWER = 70;
     public final int MEDIOCRE_ANSWER = 50;
     public final int BAD_ANSWER = 40;
@@ -23,7 +22,6 @@ public class Examiner {
     @RequiresApi(api = Build.VERSION_CODES.N)
     public String getVerdict(Challenge challenge, String userInput){
 
-        Log.d("Examiner.getVerdict():", userInput);
 
         // TODO: differentiate between subclasses of challenge
         List<String> userKeywords = Keywords.extractKeywords(userInput);
@@ -32,14 +30,11 @@ public class Examiner {
         Log.d("Examiner.getVerdict():", "userKeywords: "+userKeywords);
         Log.d("Examiner.getVerdict():", "correctKeywords: "+correctKeywords);
 
-
         List<String> missedKeywords = correctKeywords.stream().filter(k->!userKeywords.contains(k)).collect(Collectors.toList());
-
         int rememberedKeywords = correctKeywords.size() - missedKeywords.size();
         int percentageKeywords = (int)(100*rememberedKeywords/(double)correctKeywords.size());
 
         Log.d("Examiner.getVerdict():", "percentageKeywords: "+percentageKeywords);
-
 
         if(percentageKeywords <=  BAD_ANSWER){
             String s = "You're totally off track! The correct answer is: ";
@@ -53,24 +48,16 @@ public class Examiner {
 
         if(percentageKeywords <= MEDIOCRE_ANSWER){
             String mk = missedKeywords.stream().reduce((k1, k2) -> k1+", "+k2).get();
-
             return  "You got close, but you missed a lot of information. Try recalling these keywords: "+mk;
         }
 
         if(percentageKeywords <= GOOD_ANSWER){
             String mk = missedKeywords.stream().reduce((k1, k2) -> k1+", "+k2).get();
-
             return "Fair enough, but do any of these keywords tell you anything? "+mk;
         }
 
-//        if(percentageKeywords <= EXCELLENT_ANSWER){
         return "True! You guessed it!";
-//        }
-
-//        return "An error occurred in Examiner.getVerdict()!";
     }
-
-
 
 
 
