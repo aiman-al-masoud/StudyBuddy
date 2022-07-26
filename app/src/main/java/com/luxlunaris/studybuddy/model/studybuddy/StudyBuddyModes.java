c9d3ae1c6023bdb0e1a960d86c1a415258a2e485
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-07-26 17:10:26 +0200

    ...

--- app/src/main/java/com/luxlunaris/studybuddy/model/studybuddy/StudyBuddyModes.java
+++ app/src/main/java/com/luxlunaris/studybuddy/model/studybuddy/StudyBuddyModes.java
@@ -1,5 +1,9 @@
 package com.luxlunaris.studybuddy.model.studybuddy;
 
+/**
+ * Study Buddy's behavior can be modelled by a FSM,
+ * these are its states:
+ */
 public enum StudyBuddyModes {
     AWAIT_COMMAND, AWAIT_ANSWER, AWAIT_CONFIRM_TRY_AGAIN;
 }
