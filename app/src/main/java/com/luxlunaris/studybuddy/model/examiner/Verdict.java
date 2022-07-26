e10e762c9f1726d9368c1897bf30340849293ff2
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-07-26 16:42:55 +0200

    ...

--- app/src/main/java/com/luxlunaris/studybuddy/model/examiner/Verdict.java
+++ app/src/main/java/com/luxlunaris/studybuddy/model/examiner/Verdict.java
@@ -1,11 +1,7 @@
 package com.luxlunaris.studybuddy.model.examiner;
 
 public class Verdict {
-
-    public final int GOOD_ANSWER = 70;
-    public final int MEDIOCRE_ANSWER = 50;
-    public final int BAD_ANSWER = 40;
-
+    
     public boolean isFail;
     public final String text;
 
