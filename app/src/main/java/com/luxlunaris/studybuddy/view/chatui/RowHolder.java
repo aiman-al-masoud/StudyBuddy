48ce3b4bf63204748687f225828fa0ef04aad2fb
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-07-27 17:28:19 +0200

    ...

--- app/src/main/java/com/luxlunaris/studybuddy/view/chatui/RowHolder.java
+++ app/src/main/java/com/luxlunaris/studybuddy/view/chatui/RowHolder.java
@@ -1,8 +1,12 @@
 package com.luxlunaris.studybuddy.view.chatui;
 
 
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -16,6 +20,17 @@ public class RowHolder extends RecyclerView.ViewHolder {
     public RowHolder(@NonNull View itemView) {
         super(itemView);
         rowText = itemView.findViewById(R.id.rowText);
+
+
+        itemView.setOnLongClickListener(e->{
+            ClipboardManager clipboard = (ClipboardManager) itemView.getContext().getSystemService(Context.CLIPBOARD_SERVICE);
+            ClipData clip = ClipData.newPlainText("", rowText.getText());
+            clipboard.setPrimaryClip(clip);
+            Toast.makeText(e.getContext(), "Copied to clipboard", Toast.LENGTH_SHORT).show();
+            return true;
+        });
+
+
     }
 
 }
