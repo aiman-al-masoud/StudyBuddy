2e61d201fe1c10d5fc4585fc610d19e2b43d093b
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-07-28 11:20:53 +0200

    added empty FileManagerActivity

--- app/src/main/java/com/luxlunaris/studybuddy/view/ToolbarMenuClickListener.java
+++ app/src/main/java/com/luxlunaris/studybuddy/view/ToolbarMenuClickListener.java
@@ -1,12 +1,14 @@
 package com.luxlunaris.studybuddy.view;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.MenuItem;
 import android.widget.Toast;
 
 import androidx.appcompat.widget.Toolbar;
 
 import com.luxlunaris.studybuddy.R;
+import com.luxlunaris.studybuddy.view.filemanager.FileManagerActivity;
 
 public class ToolbarMenuClickListener implements Toolbar.OnMenuItemClickListener {
 
@@ -23,6 +25,10 @@ public class ToolbarMenuClickListener implements Toolbar.OnMenuItemClickListener
 
             case R.id.settingsItem:
                 Toast.makeText(context, "you clicked on settings...", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.fileManagerItem:
+                Intent i = new Intent(context, FileManagerActivity.class);
+                context.startActivity(i);
                 break;
 
         }
---
+++ app/src/main/java/com/luxlunaris/studybuddy/view/filemanager/FileManagerActivity.java
@@ -0,0 +1,15 @@
+package com.luxlunaris.studybuddy.view.filemanager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import com.luxlunaris.studybuddy.R;
+
+public class FileManagerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_file_manager);
+    }
+}
