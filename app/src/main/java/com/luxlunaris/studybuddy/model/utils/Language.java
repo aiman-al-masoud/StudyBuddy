c1b0a97da86367745c28d56105ae0c4b9bcbaafa
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-07-29 10:49:49 +0200

    ...

--- app/src/main/java/com/luxlunaris/studybuddy/model/utils/Language.java
+++ app/src/main/java/com/luxlunaris/studybuddy/model/utils/Language.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.provider.Settings;
 import android.util.Log;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AlertDialog;
 
@@ -32,9 +33,11 @@ public class Language {
     private static void unsupportedLanguageDialog(Context context){
 
         AlertDialog.Builder builder =new AlertDialog.Builder(context);
+        TextView textView = new TextView(context);
+        textView.setText("Sorry, your language is not currently supported :(\nSupported languages include: "+getAvailableLanguages().toString().replaceAll("\\[|\\]", ""));
 
         builder.setTitle("Unsupported Language");
-
+        builder.setView(textView);
 
         builder.setPositiveButton("Change Language", (d, w)->{
             goToLanguageSettings(context);
