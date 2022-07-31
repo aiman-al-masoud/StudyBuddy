6e2dbed0bab165ae16636a4cc1cc2571740d033e
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-07-31 12:11:24 +0200

    ...

--- app/src/main/java/com/luxlunaris/studybuddy/view/about/AboutActivity.java
+++ app/src/main/java/com/luxlunaris/studybuddy/view/about/AboutActivity.java
@@ -2,15 +2,44 @@ package com.luxlunaris.studybuddy.view.about;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import com.luxlunaris.studybuddy.R;
+import com.luxlunaris.studybuddy.view.help.HelpActivity;
+import com.luxlunaris.studybuddy.view.intro.IntroActivity;
 
 public class AboutActivity extends AppCompatActivity {
+
+    Button seeIntro;
+    Button license;
+    Button commandsHelp;
+    Button aboutCorpora;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+        seeIntro = findViewById(R.id.seeIntroButton);
+        license = findViewById(R.id.licenseButton);
+        commandsHelp  = findViewById(R.id.commandsHelpButton);
+        aboutCorpora = findViewById(R.id.aboutCorporaButton);
+
+
+        license.setOnClickListener(e->{
+
+        });
+
+        seeIntro.setOnClickListener(e->{
+            startActivity(new Intent(this, IntroActivity.class));
+        });
+
+        commandsHelp.setOnClickListener(e->{
+            startActivity(new Intent(this, HelpActivity.class));
+        });
+
+
     }
 }
